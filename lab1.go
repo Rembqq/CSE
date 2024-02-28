181deecf7785434c737c1caa58069501c80db07d
Anton Rak <anton.rak050@gmail.com>
2024-02-28 19:01:37 +0200

    addMain

--- lab1.go
+++ lab1.go
@@ -3,3 +3,15 @@ package main
 type TimeResponse struct {
 	Time string `json:"time"`
 }
+
+func main() {
+	http.HandleFunc("/", api)
+	server := &http.Server{
+	  Addr: ":8795",
+	}
+  
+	err := server.ListenAndServe()
+	if err != nil {
+	  log.Fatal(err)
+	}
+}
