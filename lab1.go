3e8106d2972bb6e7840413fc9453fc6338db325a
Rembqq <nikitavolkovskyi@gmail.com>
2024-02-28 21:10:32 +0200

    added quotes

--- lab1.go
+++ lab1.go
@@ -1,42 +1,42 @@
 package main
 
 import (
-  "encoding/json"
-  "log"
-  "net/http"
-  "time"
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
 )
 
 type TimeResponse struct {
-  Time string json:"time"
+	Time string `json:"time"`
 }
 
 func main() {
-  http.HandleFunc("/", api)
-  server := &http.Server{
-    Addr: ":8795",
-  }
+	http.HandleFunc("/", api)
+	server := &http.Server{
+		Addr: ":8795",
+	}
 
-  err := server.ListenAndServe()
-  if err != nil {
-    log.Fatal(err)
-  }
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func api(w http.ResponseWriter, r *http.Request) {
 
-  currentTime := time.Now().Format(time.RFC3339)
+	currentTime := time.Now().Format(time.RFC3339)
 
-  response := TimeResponse{
-    Time: currentTime,
-  }
+	response := TimeResponse{
+		Time: currentTime,
+	}
 
-  jsonData, err := json.Marshal(response)
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  }
+	jsonData, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-  w.Header().Set("Content-Type", "application/json")
-  w.Write(jsonData)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonData)
 }
