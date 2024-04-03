b602cf348f2197e0de3ca6ff3dc4c5626f703f5b
Anton Rak <anton.rak050@gmail.com>
2024-04-03 14:05:48 +0300

    Computer Handler Func

--- lab2/handler.go
+++ lab2/handler.go
@@ -1,10 +1,56 @@
 package lab2
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+)
+
 type ComputeHandler struct {
-	// TODO: Add necessary fields.
+	YourPath string
 }
 
-func (ch *ComputeHandler) Compute() error {
-	// TODO: Implement.
-	return nil
+func (ch *ComputeHandler) Compute(input, pkey, wkey string) string {
+	err := ""
+	res := 0
+
+	if pkey != "" {
+		pr := filepath.Join(ch.YourPath, pkey)
+		data, merr1 := os.ReadFile(pr)
+		if merr1 != nil {
+			err = "Помилка читання файлу"
+		} else {
+			res, err = PostfixFunc(string(data))
+			if err == "" {
+				if wkey != "" {
+					pw := filepath.Join(ch.YourPath, wkey)
+					merr2 := os.WriteFile(pw, []byte(strconv.Itoa(res)), 0666)
+					if merr2 != nil {
+						err = "Помилка запису"
+					}
+				} else {
+					fmt.Printf("Відповідь: %d", res)
+				}
+			}
+		}
+	} else {
+		if input != "" {
+			res, err = PostfixFunc(input)
+			if err == "" {
+				if wkey != "" {
+					pw := filepath.Join(ch.YourPath, wkey)
+					merr2 := os.WriteFile(pw, []byte(strconv.Itoa(res)), 0666)
+					if merr2 != nil {
+						err = "Помилка запису"
+					}
+				} else {
+					fmt.Printf("Відповідь: %d", res)
+				}
+			}
+		} else {
+			err = "Неправильно ведені данні"
+		}
+	}
+	return err
 }
