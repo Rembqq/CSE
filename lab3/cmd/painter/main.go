aaf7e12a4612a63ae94ebdb1f95d51f612f6e5de
Anton Rak <anton.rak050@gmail.com>
2024-04-21 12:03:21 +0300

    Create lab3

---
+++ lab3/cmd/painter/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/Rembqq/CSE/painter"
+	"github.com/Rembqq/CSE/painter/lang"
+	"github.com/Rembqq/CSE/ui"
+)
+
+func main() {
+	var (
+		pv ui.Visualizer // Візуалізатор створює вікно та малює у ньому.
+
+		// Потрібні для частини 2.
+		opLoop painter.Loop // Цикл обробки команд.
+		parser lang.Parser  // Парсер команд.
+	)
+
+	//pv.Debug = true
+	pv.Title = "Simple painter"
+
+	pv.OnScreenReady = opLoop.Start
+	opLoop.Receiver = &pv
+
+	go func() {
+		http.Handle("/", lang.HttpHandler(&opLoop, &parser))
+		_ = http.ListenAndServe("localhost:17000", nil)
+	}()
+
+	pv.Main()
+	opLoop.StopAndWait()
+}
