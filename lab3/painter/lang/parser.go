d25e3d46e62278f509998888cd2187eff4d54b40
Anton Rak <anton.rak050@gmail.com>
2024-05-08 13:21:12 +0300

    Add cord func

--- lab3/painter/lang/parser.go
+++ lab3/painter/lang/parser.go
@@ -3,6 +3,7 @@ package lang
 import (
 	"bufio"
 	"io"
+	"strconv"
 	"strings"
 
 	"github.com/Rembqq/CSE/painter"
@@ -12,45 +13,74 @@ import (
 type Parser struct {
 }
 
-func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
-	var res []painter.Operation           // масив операцій який передається на виконання
-	var colorWindow []painter.Operation   // масив операцій які перемальовують фон
-	var figurePicture []painter.Operation // масив операцій які малюють фігури
-	var updatePicture []painter.Operation // масив операцій які малюють фігури
-	var resetPicture []painter.Operation
+func (p *Parser) Parse(in io.Reader) ([]painter.Operation, []painter.Cord, error) {
+	var res []painter.Operation
+	var colorWindow []painter.Operation
+	var figurePicture []painter.Operation
+	var updatePicture []painter.Operation
+
+	var cordIn []painter.Cord
+	var cordWindow []painter.Cord
+	var cordFigure []painter.Cord
+	var cordUpdate []painter.Cord
 
 	scanner := bufio.NewScanner(in)
 	scanner.Split(bufio.ScanLines)
 	for scanner.Scan() {
 		commandLine := scanner.Text()
-		op := p.ParseText(commandLine) // parse the line to get Operation
+		op, x1, y1, x2, y2 := p.ParseText(commandLine)
 		switch op {
 		case "white":
 			colorWindow = append(colorWindow, painter.OperationFunc(painter.WhiteFill))
+			cordWindow = append(cordWindow, painter.Cord{X1: x1, Y1: y1, X2: x2, Y2: y2})
 		case "green":
 			colorWindow = append(colorWindow, painter.OperationFunc(painter.GreenFill))
+			cordWindow = append(cordWindow, painter.Cord{X1: x1, Y1: y1, X2: x2, Y2: y2})
 		case "bgrect":
 			figurePicture = append(figurePicture, painter.OperationFunc(painter.BlackRect))
+			cordFigure = append(cordFigure, painter.Cord{X1: x1, Y1: y1, X2: x2, Y2: y2})
 		case "figure":
 			figurePicture = append(figurePicture, painter.OperationFunc(painter.XFigureDraw))
+			cordFigure = append(cordFigure, painter.Cord{X1: x1, Y1: y1, X2: x2, Y2: y2})
 		case "move":
 		case "reset":
-			resetPicture = append(resetPicture, painter.OperationFunc(painter.BlackFill))
 		case "update":
 			updatePicture = append(updatePicture, painter.UpdateOp)
+			cordUpdate = append(cordUpdate, painter.Cord{X1: x1, Y1: y1, X2: x2, Y2: y2})
 		}
 	}
 
-	res = append(res, colorWindow...)   // в масив який передається на виконнання, додаються операції для перемалювання фону
-	res = append(res, figurePicture...) // потім в масив який передається на виконнання, додаються операції для малювання фігур
-	res = append(res, resetPicture...)
-	res = append(res, updatePicture...) // у кінець масиву передається операція оновлення текстури
+	res = append(res, colorWindow...)
+	res = append(res, figurePicture...)
+	res = append(res, updatePicture...)
 
-	return res, nil
+	cordIn = append(cordIn, cordWindow...)
+	cordIn = append(cordIn, cordFigure...)
+	cordIn = append(cordIn, cordUpdate...)
+
+	return res, cordIn, nil
 }
 
-func (p *Parser) ParseText(lineText string) string {
+func (p *Parser) ParseText(lineText string) (string, float64, float64, float64, float64) {
 	command := strings.Fields(lineText)
+	size := len(command)
 	com := command[0]
-	return com
+	x1, y1, x2, y2 := float64(0), float64(0), float64(0), float64(0)
+	if size >= 3 {
+		if command[1] != "" {
+			x1, _ = strconv.ParseFloat(command[1], 64)
+		}
+		if command[2] != "" {
+			y1, _ = strconv.ParseFloat(command[2], 64)
+		}
+	}
+	if size == 5 {
+		if command[3] != "" {
+			x2, _ = strconv.ParseFloat(command[3], 64)
+		}
+		if command[4] != "" {
+			y2, _ = strconv.ParseFloat(command[4], 64)
+		}
+	}
+	return com, x1, y1, x2, y2
 }
--- lab3/painter/op.go
+++ lab3/painter/op.go
@@ -7,10 +7,15 @@ import (
 	"golang.org/x/exp/shiny/screen"
 )
 
-// Operation змінює вхідну текстуру.
 type Operation interface {
-	// Do виконує зміну операції, повертаючи true, якщо текстура вважається готовою для відображення.
 	Do(t screen.Texture) (ready bool)
+}
+
+type Cord struct {
+	X1 float64
+	Y1 float64
+	X2 float64
+	Y2 float64
 }
 
 type OperationList []Operation
@@ -22,14 +27,14 @@ func (ol OperationList) Do(t screen.Texture) (ready bool) {
 	return
 }
 
-// UpdateOp операція, яка не змінює текстуру, але сигналізує, що текстуру потрібно розглядати як готову.
 var UpdateOp = updateOp{}
 
 type updateOp struct{}
 
-func (op updateOp) Do(t screen.Texture) bool { return true }
+func (op updateOp) Do(t screen.Texture) bool {
+	return true
+}
 
-// OperationFunc використовується для перетворення функції оновлення текстури в Operation.
 type OperationFunc func(t screen.Texture)
 
 func (f OperationFunc) Do(t screen.Texture) bool {
@@ -37,41 +42,55 @@ func (f OperationFunc) Do(t screen.Texture) bool {
 	return false
 }
 
+var CordList []Cord
+
+func PushCord(c Cord) {
+	CordList = append(CordList, c)
+}
+
+func PullCord() Cord {
+	res := CordList[0]
+	CordList[0] = Cord{X1: 0, Y1: 0, X2: 0, Y2: 0}
+	CordList = CordList[1:]
+	return res
+}
+
 func BlackFill(t screen.Texture) {
+	PullCord()
 	t.Fill(t.Bounds(), color.Black, screen.Src)
 }
 
-// WhiteFill зафарбовує тестуру у білий колір. Може бути викоистана як Operation через OperationFunc(WhiteFill).
 func WhiteFill(t screen.Texture) {
+	PullCord()
 	t.Fill(t.Bounds(), color.White, screen.Src)
 }
 
-// GreenFill зафарбовує тестуру у зелений колір. Може бути викоистана як Operation через OperationFunc(GreenFill).
 func GreenFill(t screen.Texture) {
+	PullCord()
 	t.Fill(t.Bounds(), color.RGBA{G: 0xff, A: 0xff}, screen.Src)
 }
 
-// малює червоний хрестик по координатам що вказують на його центр
 func XFigureDraw(t screen.Texture) {
-	cordinateX := int(0.5 * 800)
-	cordinateY := int(0.5 * 800)
-	xFigureWidth, xFigureHeight := 170, 70 // половини розмірів довжини та висоти прямокутника
+	c := PullCord()
+	cordinateX := int(c.X1 * 800)
+	cordinateY := int(c.Y1 * 800)
+	xFigureWidth, xFigureHeight := 170, 70
 	startX1, startY1, endX1, endY1 := cordinateX-xFigureWidth, cordinateY-xFigureHeight, cordinateX+xFigureWidth, cordinateY+xFigureHeight
 	RectFigureDraw(t, startX1, startY1, endX1, endY1, 200, 0, 0, 200)
 	startX2, startY2, endX2, endY2 := cordinateX-xFigureHeight, cordinateY-xFigureWidth, cordinateX+xFigureHeight, cordinateY+xFigureWidth
 	RectFigureDraw(t, startX2, startY2, endX2, endY2, 200, 0, 0, 200)
 }
 
-// малює чорний прямокутник по координатам що вказують
 func BlackRect(t screen.Texture) {
-	RectFigureDraw(t, 200, 200, 600, 600, 0, 0, 0, 0)
+	c := PullCord()
+	RectFigureDraw(t, int(c.X1), int(c.Y1), int(c.X2), int(c.Y2), 0, 0, 0, 0)
 }
 
 func RectFigureDraw(t screen.Texture, x1, y1, x2, y2 int, r, g, b, a byte) {
 	var pos image.Rectangle
-	pos.Min.X = x1 // лівий верхній кут, початкова координата Х
-	pos.Min.Y = y1 // лівий верхній кут, початкова координата У
-	pos.Max.X = x2 // правий нижній кут, кінцева координата Х
-	pos.Max.Y = y2 // правий нижній кут, кінцева координата У
+	pos.Min.X = x1
+	pos.Min.Y = y1
+	pos.Max.X = x2
+	pos.Max.Y = y2
 	t.Fill(pos.Bounds(), color.RGBA{R: r, G: g, B: b, A: a}, screen.Src)
 }
